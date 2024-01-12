7e0c599cabba8c3d6c71cc037317a24b35c758ee
9thdat <nthdat09@gmail.com>
2024-01-13 01:34:02 +0700

    First commit

---
+++ customer-feedback-blockchain/contracts/Token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Floopy is
+    ERC20("Floopy", "FLP"),
+    ERC20Burnable,
+    Ownable
+
+{
+    uint256 public cap = 50_000_000_000 * 10 ** uint256(18);
+    constructor(){
+        _mint(msg.sender, cap);
+        transferOwnership(msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(
+            ERC20.totalSupply() + amount <= cap,
+            "Floppy: cap exceeded"
+        );
+        _mint(to, amount);
+    }
+}
